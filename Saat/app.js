954370729a38aa3b7be0c69bb6b46ad700808668
Enes Solmaz <raidenx231@gmail.com>
2021-12-08 21:52:28 +0300

    Update app.js

--- Saat/app.js
+++ Saat/app.js
@@ -1,21 +1,21 @@
-// let text1 = document.querySelector("#myName");
+let text1 = document.querySelector("#myName");
 
-// let name = prompt("Adiniz Nedir?");
+let name = prompt("Adiniz Nedir?");
 
-// text1.innerHTML += name[0].toUpperCase() + name.substring(1);
+text1.innerHTML += name[0].toUpperCase() + name.substring(1);
 
-// function tarihSaat() {
-//     var date = new Date().toLocaleString();
-//     document.getElementById("timeUp").innerHTML = date;
-// }
+function tarihSaat() {
+    var date = new Date().toLocaleString();
+    document.getElementById("timeUp").innerHTML = date;
+}
 
-// setInterval(tarihSaat, 1000);
+setInterval(tarihSaat, 1000);
 
-// const d = new Date();
-// const day =  ["Pazartesi","Sali","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"];
+const d = new Date();
+const day =  ["Pazartesi","Sali","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"];
 
-// document.querySelector("#dday").innerHTML = day[d.getDay()];
+document.querySelector("#dday").innerHTML = day[d.getDay()];
 
-// dday.style.color  = "#d53a9d";
-// text1.style.color = "#d53a9d";
+dday.style.color  = "#d53a9d";
+text1.style.color = "#d53a9d";
 
