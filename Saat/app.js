4fb784ca26cfc76651ebc48abce1f403555c1b98
Enes Solmaz <raidenx231@gmail.com>
2021-12-08 21:50:11 +0300

    Odevler

---
+++ Saat/app.js
@@ -0,0 +1,21 @@
+// let text1 = document.querySelector("#myName");
+
+// let name = prompt("Adiniz Nedir?");
+
+// text1.innerHTML += name[0].toUpperCase() + name.substring(1);
+
+// function tarihSaat() {
+//     var date = new Date().toLocaleString();
+//     document.getElementById("timeUp").innerHTML = date;
+// }
+
+// setInterval(tarihSaat, 1000);
+
+// const d = new Date();
+// const day =  ["Pazartesi","Sali","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"];
+
+// document.querySelector("#dday").innerHTML = day[d.getDay()];
+
+// dday.style.color  = "#d53a9d";
+// text1.style.color = "#d53a9d";
+
